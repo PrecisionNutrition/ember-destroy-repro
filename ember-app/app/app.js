6e1c795a36b212688a58d92d1488e1903db0fe91
Benjamin Borowski <ben@weareokidoki.com>
2020-10-02 14:29:27 -0700

    drop dependency on single-spa-ember

--- ember-app/app/app.js
+++ ember-app/app/app.js
@@ -11,18 +11,41 @@ export default class App extends Application {
 
 loadInitializers(App, config.modulePrefix);
 
-import singleSpaEmber from 'single-spa-ember';
-
-const emberLifecycles = singleSpaEmber({
+const opts = {
   App,
   appName: 'ember-app',
   createOpts: {
     rootElement: '#ember-app',
   },
-});
+};
 
-export const bootstrap = emberLifecycles.bootstrap;
-export const mount = emberLifecycles.mount;
-export const unmount = emberLifecycles.unmount;
+export function bootstrap(opts) {
+  console.log('bootstrap');
 
+  return Promise.resolve();
+}
 
+export function mount() {
+  console.log('mount');
+
+  return Promise
+    .resolve()
+    .then(() => {
+      console.log('create');
+
+      opts.applicationInstance = opts.App.create(opts.createOpts);
+    })
+}
+
+export function unmount() {
+  console.log('unmount');
+
+  return Promise
+    .resolve()
+    .then(() => {
+      console.log('destroy');
+
+      opts.applicationInstance.destroy();
+      opts.applicationInstance = null;
+    });
+}
